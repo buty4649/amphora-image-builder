d46dd96efe6a4c854b0847dca2c320a83be95d11
Yuki Koya <ykky@pepabo.com>
2017-10-03 19:09:11 +0900

    DIB_REPO_PATHは決め打ちにしない

--- create_amphora-image.sh
+++ create_amphora-image.sh
@@ -7,7 +7,6 @@ if [ $(id -u) != 0 ];then
 fi
 
 BASEDIR="$(cd $(dirname $0); pwd)"
-export PYTHONLIB_DIR="/usr/local/lib/python2.7/dist-packages"
 DISKIMAGE_BUILDER_VERSION="2.9.0"
 OCTAVIA_VERSION=${1:-"1.0.1"}
 
@@ -25,11 +24,15 @@ apt install -y python-pip python-dib-utils python-yaml python-babel qemu libgues
 echo + install pip module
 pip install diskimage-builder==$DISKIMAGE_BUILDER_VERSION
 
-export DIB_REPO_PATH="${PYTHONLIB_DIR}/diskimage_builder"
-cd "${BASEDIR}/octavia/diskimage-create"
 
+cd "${BASEDIR}/octavia/diskimage-create"
 echo + exec diskimage-create.sh
 mkdir -p "${BASEDIR}/output"
+
+if [ -z "$DIB_REPO_PATH" ];then
+    export DIB_REPO_PATH="$(dirname $(/usr/local/lib/python2.7/dist-packages/diskimage_builder))"
+fi
+
 ./diskimage-create.sh -d xenial -o "${BASEDIR}/output/amphora-x64-haproxy.qcow2"
 chown -R $(whoami): "${BASEDIR}/output"
 
